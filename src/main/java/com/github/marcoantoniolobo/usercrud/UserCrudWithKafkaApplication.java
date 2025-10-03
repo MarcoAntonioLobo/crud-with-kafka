1ffb74183c48999d9cbfbdd8d6744b243f136452
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-10-03 20:25:57 +0100

    Project Created

---
+++ src/main/java/com/github/marcoantoniolobo/usercrud/UserCrudWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcoantoniolobo.usercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCrudWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCrudWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marcoantoniolobo/usercrud/UserCrudWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcoantoniolobo.usercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
